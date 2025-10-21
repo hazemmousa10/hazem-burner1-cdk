d81047305dc94de3dbda6da80b017b3e5890b017
Hazem Mousa <hazem@Hazems-MacBook-Air.local>
2025-10-21 22:45:41 +0300

    testing building other files in Synth

--- lib/hazem-burner1-cdk-stage.ts
+++ lib/hazem-burner1-cdk-stage.ts
@@ -1,12 +1,10 @@
 import * as cdk from "aws-cdk-lib"
 import { Construct } from "constructs";
-import { HazemBurner1CdkStack } from "./hazem-burner1-cdk-stack";
 import { EC2Stack } from "./ec2-stack";
 
 export class HazemBurner1CdkStage extends cdk.Stage {
       constructor(scope: Construct, id: string, props?: cdk.StageProps) {
         super(scope, id, props);
-
-        new EC2Stack(this, "EC2Service");
-    }
+        const ec2Stack = new EC2Stack(this, "EC2Service");
+    }   
 }
